fac5b5aa50407e4efc8d168bcdbbc4668469d8eb
Adrian Kopczynski <adriankopczynski.p@gmail.com>
2023-07-12 12:26:52 +0200

    en of section 11

--- client-app/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -1,6 +1,7 @@
 import { Grid, Icon, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/modules/activity';
 import { observer } from 'mobx-react-lite';
+import { format } from 'date-fns';
 
 interface Props {
 	activity: Activity;
@@ -25,7 +26,7 @@ const ActivityDetailedInfo = ({ activity }: Props) => {
 						<Icon name='calendar' size='large' color='teal' />
 					</Grid.Column>
 					<Grid.Column width={15}>
-						<span>{activity.date}</span>
+						<span>{format(activity.date!, 'dd MMM yyyy h:mm aa')}</span>
 					</Grid.Column>
 				</Grid>
 			</Segment>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import './app/layout/style.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
